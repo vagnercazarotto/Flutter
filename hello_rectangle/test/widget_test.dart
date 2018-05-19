5aa7f02f12a9c7d64c0233b691852e4d66b6e06a
vagnercazarotto <vagnercazarotto@gmail.com>
2018-05-19 13:21:07 +0200

    Ok, Hello world in flutter is working

--- hello_rectangle/test/widget_test.dart
+++ hello_rectangle/test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:hello_rectangle/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+    //await tester.pumpWidget(new MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -27,3 +27,4 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 }
+
