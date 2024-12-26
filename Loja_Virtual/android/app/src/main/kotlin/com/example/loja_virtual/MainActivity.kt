f6fa66da82dcac8295ec5732468f60725437fdd4
Vagner Cazarotto <vagnercazarotto@gmail.com>
2024-12-26 13:20:36 -0300

    cleaning local repo

---
+++ Loja_Virtual/android/app/src/main/kotlin/com/example/loja_virtual/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.loja_virtual
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ loja2/android/app/src/main/java/com/fluttertest/loja2/MainActivity.java
@@ -0,0 +1,6 @@
+package com.fluttertest.loja2;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
