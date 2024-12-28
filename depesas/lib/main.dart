90b43183776209f6c412bb678dc25ef5dce9bbe6
Vagner Cazarotto <vagnercazarotto@gmail.com>
2024-12-28 16:00:41 -0300

    refatoring

--- depesas/lib/main.dart
+++ depesas/lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:depesas/components/transaction_list.dart';
 import 'package:depesas/model/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+
 
 
 
@@ -58,55 +59,7 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Column(
-            children: _transactions.map((tr) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        "R\$ ${tr.value.toString()}",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                            tr.title,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                        ),
-                        Text(
-                            DateFormat('d MMM y').format(tr.date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
+          TransactionList(_transactions),
           Card(
             elevation: 5,
             child: Padding(
